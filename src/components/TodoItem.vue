0c4e8d36be286f8d89491d5dd94ae1ca4105e2a7
Viktor Daniel Varga <vvd007@gmail.com>
2024-01-11 11:00:57 +0100

    Adding events + some style fix

--- src/components/TodoItem.vue
+++ src/components/TodoItem.vue
@@ -1,19 +1,35 @@
 <script setup lang="ts">
   defineProps<{
-    checked: boolean
+    name: string,
+    checked: boolean,
+    checkStateChange: (name: string) => void,
+    removeTodo: (name: string) => void
   }>()
 
   import CheckIcon from './icons/IconCheck.vue'
 </script>
 
 <template>
-  <div class="item flex relative lg:mt-0 lg:pt-2 lg:pb-1 lg:pl-[var(--todo-gap)] lg:mb-[calc(var(--todo-gap)*0.75)]">
-    <i class="flex place-items-center place-content-center w-2 h-2 lg:left-[-26px] lg:absolute lg:rounded-[8px] lg:w-[50px] lg:h-[50px] lg:top-[calc(50%-25px)] lg:border lg:border-solid lg:border-[var(--color-border)] lg:bg-[var(--color-background)] text-[var(--color-text)]">
+  <div class="item flex relative lg:mt-0 lg:pt-2 lg:pb-1 lg:pl-[var(--todo-gap)] lg:mb-[calc(var(--todo-gap)*0.75)] group">
+    <i 
+      class="flex place-items-center place-content-center w-2 h-2 lg:left-[-26px] lg:absolute lg:rounded-[8px] lg:w-[50px] lg:h-[50px] lg:top-[calc(50%-25px)] lg:border lg:border-solid lg:border-[var(--color-border)] lg:bg-[var(--color-background)] text-[var(--color-text)]"
+      v-on:click="() => checkStateChange(name)"  
+    >
       <CheckIcon v-if="checked"/>
     </i>
     <div class="details flex-1 ml-4">
-      <h3 class="text-xl font-medium text-[var(--color-heading)]">
-        <slot name="heading"></slot>
+      <h3 class="text-xl font-medium text-[var(--color-heading)] flex gap-2 items-center">
+        {{ name }}
+        <svg 
+          xmlns="http://www.w3.org/2000/svg" 
+          height="16" 
+          width="12" 
+          viewBox="0 0 384 512" 
+          class="hover:cursor-pointer relative top-[2.5px]"
+          v-on:click="() => removeTodo(name)"
+        >
+          <path class="fill-red-600 hidden group-hover:inline-block" d="M345 137c9.4-9.4 9.4-24.6 0-33.9s-24.6-9.4-33.9 0l-119 119L73 103c-9.4-9.4-24.6-9.4-33.9 0s-9.4 24.6 0 33.9l119 119L39 375c-9.4 9.4-9.4 24.6 0 33.9s24.6 9.4 33.9 0l119-119L311 409c9.4 9.4 24.6 9.4 33.9 0s9.4-24.6 0-33.9l-119-119L345 137z"/>
+        </svg>
       </h3>
     </div>
   </div>
--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -3,12 +3,14 @@
 
   import TodoItem from './TodoItem.vue';
 
-  // STATES
-  let newTodoText = ref('');
-  const todos = ref<{
+  interface Todo {
     name: string, 
     checked: boolean
-  }[]>([
+  };
+
+  // STATES
+  let newTodoText = ref('');
+  let todos = ref<Todo[]>([
     { name: 'Documentation', checked: true },
     { name: 'Tooling', checked: true },
     { name: 'Ecosystem', checked: true },
@@ -20,25 +22,29 @@
 
   // EVENTS
   function addTodo() {
-    console.log(`Now I should add ${newTodoText.value}`);
+    todos.value.push({ name: newTodoText.value, checked: false });
 
     newTodoText.value = '';
+  }
+  function removeTodo(name: string) {
+    todos.value = todos.value.filter(todo => todo.name !== name);
+  }
+  function checkStateChange(name: string) {
+    todos.value = todos.value.map((todo:Todo) => ({...todo, checked: (todo.name === name ? !todo.checked : todo.checked)}));
   }
 </script>
 
 <template>
-  <div class="flex align-stretch mb-5 ml-[-25px]">
+  <div class="flex align-stretch mb-5 ml-[-25px] gap-1">
     <input 
       v-model="newTodoText" 
       class="w-full rounded-md border-0 py-1.5 px-3 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-teal-400 focus-visible:outline-none"
     >
     <button 
       @click="addTodo"
-      class="flex justify-center items-center py-1 px-4 ml-2 rounded-md border border-slate-300 rounded border-solid bg-teal-400 hover:bg-teal-600 text-white text-4xl"
+      class="flex justify-center items-center pt-2 pb-3 px-4 ml-2border border-slate-300 rounded border-solid bg-teal-400 hover:bg-teal-600 text-white text-4xl leading-none"
       >+</button>
   </div>
 
-  <TodoItem v-for="todo in todos" :checked="todo.checked">
-    <template #heading>{{ todo.name }}</template>
-  </TodoItem>
+  <TodoItem v-for="todo in todos" :checked="todo.checked" :name="todo.name" :check-state-change="checkStateChange" :remove-todo="removeTodo"/>
 </template>
