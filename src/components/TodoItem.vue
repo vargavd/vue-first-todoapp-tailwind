24fc6cca127691349e7a0c0d52b082481cce4fab
Viktor Daniel Varga <vvd007@gmail.com>
2024-01-11 04:04:55 +0100

    Define todos and pass as props

--- src/components/TodoItem.vue
+++ src/components/TodoItem.vue
@@ -1,7 +1,15 @@
+<script setup lang="ts">
+  defineProps<{
+    checked: boolean
+  }>()
+
+  import CheckIcon from './icons/IconCheck.vue'
+</script>
+
 <template>
   <div class="item flex relative lg:mt-0 lg:pt-2 lg:pb-1 lg:pl-[var(--todo-gap)] lg:mb-[calc(var(--todo-gap)*0.75)]">
     <i class="flex place-items-center place-content-center w-2 h-2 lg:left-[-26px] lg:absolute lg:rounded-[8px] lg:w-[50px] lg:h-[50px] lg:top-[calc(50%-25px)] lg:border lg:border-solid lg:border-[var(--color-border)] lg:bg-[var(--color-background)] text-[var(--color-text)]">
-      <slot name="icon"></slot>
+      <CheckIcon v-if="checked"/>
     </i>
     <div class="details flex-1 ml-4">
       <h3 class="text-xl font-medium text-[var(--color-heading)]">
--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -1,11 +1,22 @@
 <script setup lang="ts">
   import { ref } from 'vue';
 
-  import TodoItem from './TodoItem.vue'
-  import CheckIcon from './icons/IconCheck.vue'
+  import TodoItem from './TodoItem.vue';
 
   // STATES
   let newTodoText = ref('');
+  const todos = ref<{
+    name: string, 
+    checked: boolean
+  }[]>([
+    { name: 'Documentation', checked: true },
+    { name: 'Tooling', checked: true },
+    { name: 'Ecosystem', checked: true },
+    { name: 'Community', checked: true },
+    { name: 'Support Vue', checked: true },
+    { name: 'Misc Todo', checked: false },
+    { name: 'Learn Vue', checked: false },
+  ]);
 
   // EVENTS
   function addTodo() {
@@ -27,68 +38,7 @@
       >+</button>
   </div>
 
-  <TodoItem>
-    <template #icon>
-      <CheckIcon />
-    </template>
-    <template #heading>Documentation</template>
-
-    Vue’s
-    <a href="https://vuejs.org/" target="_blank" rel="noopener">official documentation</a>
-    provides you with all information you need to get started.
-  </TodoItem>
-
-  <TodoItem>
-    <template #icon>
-      <CheckIcon />
-    </template>
-    <template #heading>Tooling</template>
-
-    More instructions are available in <code>README.md</code>.
-  </TodoItem>
-
-  <TodoItem>
-    <template #icon>
-      <CheckIcon />
-    </template>
-    <template #heading>Ecosystem</template>
-
-    Get official tools and libraries for your project:
-    <a href="https://pinia.vuejs.org/" target="_blank" rel="noopener">Pinia</a>,
-    <a href="https://router.vuejs.org/" target="_blank" rel="noopener">Vue Router</a>,
-    <a href="https://test-utils.vuejs.org/" target="_blank" rel="noopener">Vue Test Utils</a>, and
-    <a href="https://github.com/vuejs/devtools" target="_blank" rel="noopener">Vue Dev Tools</a>. If
-    you need more resources, we suggest paying
-    <a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">Awesome Vue</a>
-    a visit.
-  </TodoItem>
-
-  <TodoItem>
-    <template #icon>
-      <CheckIcon />
-    </template>
-    <template #heading>Community</template>
-
-    Got stuck? Ask your question on
-    <a href="https://chat.vuejs.org" target="_blank" rel="noopener">Vue Land</a>, our official
-    Discord server, or
-    <a href="https://stackoverflow.com/questions/tagged/vue.js" target="_blank" rel="noopener"
-      >StackOverflow</a
-    >. You should also subscribe to
-    <a href="https://news.vuejs.org" target="_blank" rel="noopener">our mailing list</a> and follow
-    the official
-    <a href="https://twitter.com/vuejs" target="_blank" rel="noopener">@vuejs</a>
-    twitter account for latest news in the Vue world.
-  </TodoItem>
-
-  <TodoItem>
-    <template #icon>
-      <CheckIcon />
-    </template>
-    <template #heading>Support Vue</template>
-
-    As an independent project, Vue relies on community backing for its sustainability. You can help
-    us by
-    <a href="https://vuejs.org/sponsor/" target="_blank" rel="noopener">becoming a sponsor</a>.
+  <TodoItem v-for="todo in todos" :checked="todo.checked">
+    <template #heading>{{ todo.name }}</template>
   </TodoItem>
 </template>
